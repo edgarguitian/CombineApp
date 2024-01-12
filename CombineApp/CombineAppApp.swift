a5f90a16f3b5833b954fd7c224b8b4849afd7162
Edgar <edguitian@imagine800.es>
2024-01-12 12:45:38 +0100

    Initial Commit

---
+++ CombineApp/CombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppApp.swift
+//  CombineApp
+//
+//  Created by Edgar Guitian Rey on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
